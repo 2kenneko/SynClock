1f1a26f5f2fc25e188744e98caffa1c663275933
2k2e2n <98691163+2k2e2n@users.noreply.github.com>
2024-08-26 02:57:51 +0900

    Update Template.tsx

--- assets/Dev/Template.tsx
+++ assets/Dev/Template.tsx
@@ -1,36 +1,15 @@
-// vueの index.vueと同じ
-
-"use client";
-import Image from "next/image";
-import { useEffect, useState, useRef } from "react";
-import styles from "./css/page.module.css";
-import Showtime from "@/components/time-render/Showtime";
+import Showtime from '@/components/timer';
+import { useState } from 'react';
 
 export default function Page() {
-  // SCRIPT SETUP
 
-  /* 変数定義のルール
-  - わかりやすい名前
-  - ＿の後は大文字
-  - 初期状態を保存しておく
-
-  - Boolは_Stage
-  - numberは_Value
-  - stringは_Str
-*/
-
-  let [task_Str, settask_Str]                     = useState<string>  (""); //タスクの名前
-
-
-  useEffect(() => {
-    //最初のみ実行
-
-  }, []);
-
+  let [time, settime] = useState<number>(0);
+  const [playername, setplayername] = useState<string>("Bob");
+  const rest_time : number = 100;
 
   return (
     <main>
-
+      <Showtime time={time} />
     </main>
   );
 }
