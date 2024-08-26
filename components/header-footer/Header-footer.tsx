711f152ecdc8b142ac3ff96ab470a62210863c4d
2k2e2n <98691163+2k2e2n@users.noreply.github.com>
2024-08-26 20:08:08 +0900

    add Overlay CSS

--- components/header-footer/Header-footer.tsx
+++ components/header-footer/Header-footer.tsx
@@ -1,31 +1,38 @@
-"use client";
-import Image from "next/image";
-import { useEffect, useState, useRef } from "react";
-import styles from "./header_footer.module.css";
-import { useRouter } from "next/navigation";
-
+'use client';
+import Image from 'next/image';
+import { useEffect, useState, useRef, FC, useCallback } from 'react';
+import styles from './header_footer.module.css';
+import { useRouter } from 'next/navigation';
+import landscape_screen from '@/assets/images/landscape-screen.gif';
 
 export default function Page() {
   const router = useRouter();
-  const link_top : string = "/";
-  const link_todo : string = "/todo";
-  const link_whatstudy : string = "/whatstudy";
+  const link_top: string = '/';
+  const link_todo: string = '/todo';
+  const link_whatstudy: string = '/whatstudy';
 
-  function top_link() {router.push(link_top);};
-  function todo_link() {router.push(link_todo);};
-  function whatstudy_link() {router.push(link_whatstudy);};
+  let [isScreenVertical_Bool, setisScreenVertical_Bool] = useState<boolean>(false);
+  function top_link() {
+    router.push(link_top);
+  }
+  function todo_link() {
+    router.push(link_todo);
+  }
+  function whatstudy_link() {
+    router.push(link_whatstudy);
+  }
 
   return (
     <main>
       <div>
         <header className={styles.header}>
-          <div className={styles["header-left"]}>
-            <h1 className={styles["header-text"]}>タイマーテスト</h1>
+          <div className={styles['header-left']}>
+            <h1 className={styles['header-text']}>タイマーテスト</h1>
           </div>
-          <div className={styles["header-right"]}>
-            <div className={styles["header-btn"]}>
+          <div className={styles['header-right']}>
+            <div className={styles['header-btn']}>
               <div>
-                <button onClick={top_link} className={styles.button} >
+                <button onClick={top_link} className={styles.button}>
                   <span className={styles.lable}>TOP</span>
                 </button>
               </div>
@@ -45,6 +52,9 @@ export default function Page() {
               <button className={styles.button}>
                 <span className={styles.lable}>Darkmode</span>
               </button>
+            </div>
+            <div className={styles.overlay}>
+              <Image src={landscape_screen.src} layout="responsive" width={1} height={1} alt="" loading="lazy" className={styles.landscape_screen} />
             </div>
           </div>
         </header>
