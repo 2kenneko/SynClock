01da98052742d2dd9167fa7901ac2a7dbf498297
2k2e2n <98691163+2k2e2n@users.noreply.github.com>
2024-09-15 18:21:06 +0900

    update

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,9 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false, // Prevents double rendering in development mode
+  output: 'export',
+  distDir: 'dist',
+  trailingSlash: true,
   webpack(config) {
     // Add alias for paths
     config.resolve.alias['@'] = path.join(__dirname, 'src');
