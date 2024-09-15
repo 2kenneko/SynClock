d20116fac9ad0d5fc381f7dd58103753af4be8c1
2k2e2n <98691163+2k2e2n@users.noreply.github.com>
2024-09-15 17:52:56 +0900

    update

--- next.config.mjs
+++ next.config.mjs
@@ -6,12 +6,15 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false, //２回実行されるのを防ぐ
+  reactStrictMode: false, // Prevents double rendering in development mode
+  distDir: 'build', // Set custom build directory
   webpack(config) {
+    // Add alias for paths
     config.resolve.alias['@'] = path.join(__dirname, 'src');
-
+    
+    // Add rule for handling mp3 files
     config.module.rules.push({
-      test: /\.(mp3)$/,
+      test: /\.(mp3)$/i, // mp3 files
       type: "asset/resource",
       generator: {
         filename: "static/chunks/[path][name].[hash][ext]",
